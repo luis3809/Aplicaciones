13001b3182c1572b0f6e1b53579ac84533143ad8
luis3809 <luicord@gmail.com>
2026-06-02 19:50:56 -0500

    cambios realizados en app.py y requirements.txt para mejorar la aplicación de clustering K-Means, incluyendo la adición de nuevas dependencias y ajustes en la interfaz de usuario.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.markdown("""
     <div style='background-color:#0A1F44;padding:18px;border-radius:8px;margin-bottom:20px'>
         <h2 style='color:white;margin:0;'>🔵 Analiza los datos de un SCADA</h2>
         <p style='color:#D9E1F2;margin:0;font-size:16px;'>
-            Convierte esa curiosidad en interacción real. Carga tu archivo SCADA y descubre los modos de operación de tu equipo. El formato de tu archivo csv (numérico) max 1000 filas y 7 columnas.
+            Convierte esa curiosidad en interacción real. Carga tu archivo extraido de un SCADA y descubre los modos de operación de tu equipo. El formato de tu archivo csv (numérico) max 1000 filas y 7 columnas.
     </div>
 """, unsafe_allow_html=True)
 
